727eb015f4917d37cc5356a910d4ff5446414572
Jon Telles <jongtelles@gmail.com>
2021-01-20 17:41:10 -0500

    added github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,12 @@ function Footer() {
     <footer className='footer'>
       <p className='footer--copyright'>© Jon Telles {time}</p>
       <p className='footer--blurb'>🙃 Made by a human being 🙃</p>
+      <a
+        className='gh--link'
+        href='https://github.com/Jongtelles/game-of-life-viz'
+      >
+        <span>Github Link</span>
+      </a>
     </footer>
   );
 }
